26d03cb11cb4dc1f6e96350d7b0edd4eec048315
MayerlyBR <bustosrobayom@gmail.com>
2025-06-07 11:33:02 -0500

    feat(constants): añadir constantes para mensajes de exceptions

--- src/main/java/com/devs/product/api/util/Constants.java
+++ src/main/java/com/devs/product/api/util/Constants.java
@@ -6,4 +6,10 @@ public class Constants {
     public static final String UNEXPECTED_ERROR = "Ha ocurrido un error inesperado";
 
     public static final String DB_ERROR = "Ha ocurrido un error al guardar el producto en BBDD.";
+
+    public static final String ERROR_LIST = "Ha ocurrido un error al obtener los productos de la BBDD";
+
+    public static final String NOT_FOUND_PARAMETER = "Los parameters page y size son obligatorios";
+
+    public static final String PATH_NOT_FOUND = "Path no válido";
 }

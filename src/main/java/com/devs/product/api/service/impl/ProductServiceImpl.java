9fd333421f4f1f77f2d670446a87b757c51d4afc
MayerlyBR <bustosrobayom@gmail.com>
2025-06-07 08:12:55 -0500

    feat(product): crear lógica para guardar Product

--- src/main/java/com/devs/product/api/service/impl/ProductServiceImpl.java
+++ src/main/java/com/devs/product/api/service/impl/ProductServiceImpl.java
@@ -1,9 +1,12 @@
 package com.devs.product.api.service.impl;
 
 import com.devs.product.api.dto.ProductDTO;
+import com.devs.product.api.exception.AppBaseException;
 import com.devs.product.api.mapper.ProductMapper;
+import com.devs.product.api.model.Product;
 import com.devs.product.api.repository.ProductRepository;
 import com.devs.product.api.service.IProductService;
+import com.devs.product.api.util.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,9 +22,22 @@ public class ProductServiceImpl implements IProductService {
 
     @Autowired
     private ProductMapper mapper;
+
     @Override
     public ProductDTO createProduct(ProductDTO productDTO) {
-        return null;
+        try {
+
+            if (productDTO == null) {
+                return null;
+            }
+            Product productSave = mapper.toEntity(productDTO);
+            Product product = productRepository.save(productSave);
+            return mapper.toDTO(product);
+
+        } catch (Exception ex) {
+            log.error(Constants.DB_ERROR, ex, ex.getMessage());
+            throw new AppBaseException(Constants.DB_ERROR);
+        }
     }
 
     @Override

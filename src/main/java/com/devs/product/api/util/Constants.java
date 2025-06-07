fcff1f0cba7adbaf4c6dc1391cb542906d7098af
MayerlyBR <bustosrobayom@gmail.com>
2025-06-07 08:21:34 -0500

    feat(constants): crear clase constantes para mensajes y configuraciones comunes

---
+++ src/main/java/com/devs/product/api/util/Constants.java
@@ -0,0 +1,9 @@
+package com.devs.product.api.util;
+
+
+public class Constants {
+
+    public static final String UNEXPECTED_ERROR = "Ha ocurrido un error inesperado";
+
+    public static final String DB_ERROR = "Ha ocurrido un error al guardar el producto en BBDD.";
+}

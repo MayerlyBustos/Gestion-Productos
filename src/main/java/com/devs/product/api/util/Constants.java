4043925a026ce39572ee99c2121d5ca5f2f3ad35
MayerlyBR <bustosrobayom@gmail.com>
2025-06-07 20:30:27 -0500

    feat(constants): añadir constantes para los mensajes de exceptiones

--- src/main/java/com/devs/product/api/util/Constants.java
+++ src/main/java/com/devs/product/api/util/Constants.java
@@ -12,4 +12,14 @@ public class Constants {
     public static final String NOT_FOUND_PARAMETER = "Los parameters page y size son obligatorios";
 
     public static final String PATH_NOT_FOUND = "Path no válido";
+
+    public static final String PRODUCT_NOT_FOUND = "El producto no existe";
+
+    public static final String ERROR_GET_PRODUCT = "Ha ocurrido un error al obtener el producto de la BBDD";
+
+    public static final String NOT_VALID = "Parámetros de paginación no válidos";
+
+    public static final String NON_NULL_PRODUCT = "El producto no puede ser nulo";
+
+    public static final String ERROR_ARGUMENT_TYPE = "Tipo de dato no válido";
 }

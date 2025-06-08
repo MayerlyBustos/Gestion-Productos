c6da29843bd7da4d6684c0e5b3050bf2242b5c59
MayerlyBR <bustosrobayom@gmail.com>
2025-06-08 08:02:55 -0500

    style(exception): formatear espacios en clase NotFoundProductException

--- src/main/java/com/devs/product/api/exception/NotFoundProductException.java
+++ src/main/java/com/devs/product/api/exception/NotFoundProductException.java
@@ -1,6 +1,6 @@
 package com.devs.product.api.exception;
 
-public class NotFoundProductException extends AppBaseException{
+public class NotFoundProductException extends AppBaseException {
     public NotFoundProductException(String message) {
         super(message);
     }

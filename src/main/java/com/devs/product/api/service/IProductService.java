173be9077b9e1ae0c8266bd96aa2f57c10220176
MayerlyBR <bustosrobayom@gmail.com>
2025-06-08 08:07:01 -0500

    refactor(service): agregar parámetro 'productId' al método updateProduct

--- src/main/java/com/devs/product/api/service/IProductService.java
+++ src/main/java/com/devs/product/api/service/IProductService.java
@@ -2,9 +2,6 @@ package com.devs.product.api.service;
 
 import com.devs.product.api.dto.PageResponse;
 import com.devs.product.api.dto.ProductDTO;
-import org.springframework.data.domain.Page;
-
-import java.util.List;
 
 public interface IProductService {
 
@@ -14,7 +11,7 @@ public interface IProductService {
 
     ProductDTO getProductById(Long productId);
 
-    ProductDTO updateProduct(ProductDTO productDTO);
+    ProductDTO updateProduct(Long productId, ProductDTO productDTO);
 
     void deleteProductById(Long productId);
 }

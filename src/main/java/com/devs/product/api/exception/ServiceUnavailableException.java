7f31663533f49d85ad857f203ded3a0c1f3a7687
MayerlyBR <bustosrobayom@gmail.com>
2025-06-08 07:59:25 -0500

    feat(exception): crear nueva clase ServiceUnavailableException para manejar la indisponibilidad de la base de datos

---
+++ src/main/java/com/devs/product/api/exception/ServiceUnavailableException.java
@@ -0,0 +1,7 @@
+package com.devs.product.api.exception;
+
+public class ProductDataException extends AppBaseException {
+    public ProductDataException(String message) {
+        super(message);
+    }
+}

9031a257f1f5f319ba5a1af9f2b821d7140af2c6
MayerlyBR <bustosrobayom@gmail.com>
2025-06-08 21:20:15 -0500

    featchore(deps): Agregar dependencia de redis

--- src/main/java/com/devs/product/api/util/Constants.java
+++ src/main/java/com/devs/product/api/util/Constants.java
@@ -5,10 +5,6 @@ public class Constants {
 
     public static final String UNEXPECTED_ERROR = "Ha ocurrido un error inesperado";
 
-    public static final String DB_ERROR = "Ha ocurrido un error al guardar el producto en BBDD.";
-
-    public static final String ERROR_LIST = "Ha ocurrido un error al obtener los productos de la BBDD";
-
     public static final String NOT_FOUND_PARAMETER = "Los parameters page y size son obligatorios";
 
     public static final String PATH_NOT_FOUND = "Path no válido";
@@ -22,4 +18,12 @@ public class Constants {
     public static final String NON_NULL_PRODUCT = "El producto no puede ser nulo";
 
     public static final String ERROR_ARGUMENT_TYPE = "Tipo de dato no válido";
+
+    public static final String ERROR_ACCESS_BBDD = "Base de datos no disponible";
+
+    public static final String INVALID_PRODUCT_ID = "El id debe ser un número positivo y diferente de nulo";
+
+    public static final String SERVICE_UNAVAILABLE = "Servicio no disponible. Por favor, contacte con el administrador";
+
+    public static final Object GROUP = "group";
 }
